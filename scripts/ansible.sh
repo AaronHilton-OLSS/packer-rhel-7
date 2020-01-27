00a0f4b71163b9c124b23f57127b5cd86ca9d5da
AaronHilton-OLSS <aaron.hilton@essilorusa.com>
2020-01-27 12:06:10 -0600

    Adding vagrant keys, but I still don't think this is enough to get the vagrant box connecting properly

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -3,7 +3,7 @@
 # Configure repos
 subscription-manager register --username=${RHSM_USERNAME} --password=${RHSM_PASSWORD} --name=packer-rhel7-$(date +%Y%m%d)-${RANDOM}
 subscription-manager attach --auto
-subscription-manager repos --disable=*
+#subscription-manager repos --disable=*
 subscription-manager repos --enable=rhel-7-server-ansible-2.9-rpms --enable=rhel-7-server-rpms
 
 # Update to last patches
@@ -15,3 +15,11 @@ yum history package ansible|awk '/Install/ {print $1}' > /tmp/YUM_ID
 
 # Configure /tmp on tmpfs
 systemctl enable tmp.mount
+
+# Installing vagrant keys
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+cd ~/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+chown -R vagrant ~/.ssh
